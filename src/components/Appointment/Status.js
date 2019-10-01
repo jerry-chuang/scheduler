efc62919e08c679bcf96bdc12490950389767f8e
Jerry Chuang <48072980+JerChuang@users.noreply.github.com>
2019-10-01 10:15:41 -0700

    Remove unused import

--- src/components/Appointment/Status.js
+++ src/components/Appointment/Status.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "components/Appointment/styles.scss";
-import Button from "components/Button.js";
 
 export default function Status(props) {
   const {message} = props;
