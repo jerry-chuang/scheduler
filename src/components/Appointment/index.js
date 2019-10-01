6665d1e8635ceac1e5ce81f55a5ce69fef41e3c9
Jerry Chuang <48072980+JerChuang@users.noreply.github.com>
2019-10-01 09:28:25 -0700

    Add Appointment component style and index

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "components/Appointment/styles.scss";
+import classNames from 'classnames/bind';
+
+export default function Appointment(props) {
+   
+
+   return <article className="appointment"></article>;
+}
+
