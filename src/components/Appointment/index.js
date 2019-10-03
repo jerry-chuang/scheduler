5cd42d21083f8dbe4ff9d9497798495bb8af1380
Jerry Chuang <48072980+JerChuang@users.noreply.github.com>
2019-10-03 09:18:26 -0700

    Add additional modes

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,18 +4,58 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header"
 import Empty from "components/Appointment/Empty"
 import Show from "components/Appointment/Show"
-import classNames from 'classnames/bind';
+import useVisualMode from "hooks/useVisualMode";
+import Form from "components/Appointment/Form"
 
 export default function Appointment({time, interview}) {
-   return <article className="appointment">
-      <>
-      <Header time={time}/>
-      {interview ?
-         <Show 
-            student={interview.student}
-            interviewer={interview.interviewer}
-         />:
-         <Empty />}
-      </>
-   </article>;
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const SAVING = "SAVING";
+  const EDIT = "EDIT";
+  const DELETING = "DELETING";
+  const CONFIRM = "CONFIRM";
+
+  const {mode, transition, back} = useVisualMode(interview? SHOW : EMPTY)
+  const onAdd = () => {
+    transition(CREATE);
+  };
+  const onSave = () => {
+    transition(SAVING);
+  };
+  const onCancel = () => {
+    back();
+  }
+  const onConfirm = () => {
+    transition(DELETING);
+  };
+  const onDelete = () => {
+    transition(CONFIRM);
+  };
+  const onComplete = () => {
+    transition(EMPTY);
+  };
+  const onEdit = () => {
+    transition(EDIT);
+  };
+
+  return <article className="appointment">
+    <Header time={time}/>
+    {mode === EMPTY && <Empty onAdd={onAdd} />}
+    {mode === SHOW && (
+      <Show
+        student={interview.student}
+        interviewer={interview.interviewer}
+      />
+    )} 
+    {mode === CREATE && (
+      <Form
+        name={interview && interview.student}
+        interviewers={[]}
+        onSave={onSave}
+        onCancel={onCancel}
+        interviewer={interview && interview.interviewer}
+      />)}   
+  </article>;
 }
