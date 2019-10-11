3ad194172bc5b6a72a6f412266eed340846c0be6
Jerry Chuang <48072980+JerChuang@users.noreply.github.com>
2019-10-10 18:18:55 -0700

    Implement error catching with TDD

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -13,6 +13,7 @@ export default function Form(props) {
   } = props;
   const [name, setName] = useState(propName || "");
   const [interviewer, setInterviewer] = useState(propInterviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setName("");
@@ -23,6 +24,13 @@ export default function Form(props) {
     reset();
     onCancel();
   };
+  const validate = () => {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    onSave(name, interviewer);
+  };
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -32,23 +40,25 @@ export default function Form(props) {
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
-            placeholder="Student Name"
+            placeholder="Enter Student Name"
             value={name}
             onChange={event => setName(event.target.value)}
+            data-testid="student-name-input"
+          />
+          <section className="appointment__validation">{error}</section>
+          <InterviewerList
+            interviewers={interviewers}
+            value={interviewer}
+            onChange={setInterviewer}
           />
         </form>
-        <InterviewerList
-          interviewers={interviewers}
-          value={interviewer}
-          onChange={setInterviewer}
-        />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button onClick={cancel} danger>
             Cancel
           </Button>
-          <Button onClick={() => onSave(name, interviewer)} confirm>
+          <Button onClick={() => validate()} confirm>
             Save
           </Button>
         </section>
